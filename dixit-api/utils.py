c7b24bdc41308eed9200bd2e4cdb2eb171aa6d66
carolinux <carolinegr@gmail.com>
2022-07-30 20:30:41 +0200

    Make cookie persistent accross browser restarts

--- dixit-api/utils.py
+++ dixit-api/utils.py
@@ -28,8 +28,8 @@ def authenticate_with_cookie_token(f):
     return decorated_function
 
 
-def create_token(player_name, gid):
-    return jwt.encode({'public_ids': player_name, 'gids': gid, 'exp': datetime.datetime.now() + datetime.timedelta(minutes=120)},
+def create_token(player_name, gid, expiration_date):
+    return jwt.encode({'public_ids': player_name, 'gids': gid, 'exp': expiration_date},
                        conf.secret_key, algorithm="HS256")
 
 
@@ -45,16 +45,17 @@ def get_games_from_cookie(request):
 def generate_response_with_jwt_token(request, response, player_name, gid):
     """Generate a jwt token which encodes all the games the person is a member of (usually will be only one)."""
     cookies = request.cookies.to_dict()
+    expiration_date = datetime.datetime.utcnow() + datetime.timedelta(minutes=120)
     if not cookies.get('token'):
-        response.set_cookie("players", player_name, httponly=True, samesite='Strict')
-        response.set_cookie("gids", gid, httponly=True, samesite='Strict')
-        response.set_cookie("token", create_token(player_name, gid), httponly=True, samesite='Strict')
+        response.set_cookie("players", player_name, httponly=True, samesite='Strict', expires=expiration_date)
+        response.set_cookie("gids", gid, httponly=True, samesite='Strict', expires=expiration_date)
+        response.set_cookie("token", create_token(player_name, gid, expiration_date=expiration_date), httponly=True, samesite='Strict', expires=expiration_date)
     else:
         existing_names = cookies.get('players')
         existing_games = cookies.get('gids')
         new_names = existing_names + ',' + player_name
         new_games = existing_games + ',' + gid
-        response.set_cookie("players", new_names, httponly=True, samesite='Strict')
-        response.set_cookie("gids", new_games, httponly=True, samesite='Strict')
-        response.set_cookie("token", create_token(new_names, new_games), httponly=True, samesite='Strict')
+        response.set_cookie("players", new_names, httponly=True, samesite='Strict', expires=expiration_date)
+        response.set_cookie("gids", new_games, httponly=True, samesite='Strict', expires=expiration_date)
+        response.set_cookie("token", create_token(new_names, new_games, expiration_date=expiration_date), httponly=True, samesite='Strict', expires=expiration_date)
     return response
