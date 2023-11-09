c8a47baa9cb2b9ade2c6acf01068c975e4f439d2
carolinux <carolinegr@gmail.com>
2023-11-09 22:46:14 +0100

    add more cute ids

--- dixit-api/cute_ids.py
+++ dixit-api/cute_ids.py
@@ -1,9 +1,10 @@
 import random
 
 adjectives = ['fluffy', 'chonky', 'yellow', 'red', 'blue', 'shiny', 'light', 'bouncy', 'clever', 'ferocious', 'fiery',
-              'frosty', 'electric', 'squishy', 'purple', 'majestic']
+              'frosty', 'electric', 'squishy', 'purple', 'majestic', 'happy', 'sleepy', 'sunny', 'chocolate', 'green', 'orange']
 animals = ['bird', 'butterfly', 'ladybug', 'dolphin', 'kitten', 'puppy', 'rhino', 'bear', 'mouse', 'elephant',
-           'squirrel', 'cheese', 'watermelon', 'feather']
+           'squirrel', 'cheese', 'watermelon', 'feather', 'bee', 'cat', 'dog', 'fish', 'frog', 'horse', 'lion', 'mushroom']
+
 
 
 def generate_cute_id():
